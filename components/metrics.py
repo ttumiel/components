b65fe68763208a5ee9cd373c8067e984b9fb8352
Thomas Tumiel <ttumiel@gmail.com>
2020-04-28 15:04:52 +0200

    add quadratic kappa metric

--- components/metrics.py
+++ components/metrics.py
@@ -1,4 +1,7 @@
 import torch
+from sklearn.metrics import cohen_kappa_score
+
+
 def accuracy(preds, target):
     """
     The average number of correct classes. Only useful
@@ -7,3 +10,13 @@ def accuracy(preds, target):
     """
     assert preds.shape == target.shape, f"preds.shape ({preds.shape}) != target.shap ({target.shape})"
     return torch.mean((preds==target).float())
+
+
+def quadratic_kappa(preds, target):
+    """
+    Quadratic kappa score. Used for regression tasks to quadratically penalize
+    values that are far from the correct answer. Works well to test inter-
+    observer variability. See https://en.wikipedia.org/wiki/Cohen%27s_kappa
+    """
+    assert preds.shape == target.shape, f"preds.shape ({preds.shape}) != target.shap ({target.shape})"
+    return torch.tensor(cohen_kappa_score(preds.detach().cpu().numpy(), target.detach().cpu().numpy(), weights='quadratic'))
