aebf22c79ab67dd0c83739213955a87d9ea45a74
Thomas Tumiel <ttumiel@gmail.com>
2020-05-01 13:58:23 +0200

    add image utils

---
+++ components/images.py
@@ -0,0 +1,28 @@
+import skimage.io
+from PIL import Image
+import numpy as np
+
+def open_tiff_image(path, level=2):
+    """
+    Opens a .tiff image. These images can have multiple
+    'levels', like sizes of the image.
+    """
+    return skimage.io.MultiImage(str(path))[level]
+
+def pad_to_square(im, min_size=256, fill_color=(0,0,0)):
+    "Pad a PIL Image to a square."
+    x, y = im.size
+    size = max(min_size, x, y)
+    new_im = Image.new('RGB', (size, size), fill_color)
+    new_im.paste(im, (int((size - x) / 2), int((size - y) / 2)))
+    return new_im
+
+def crop_to_mask(im, mask):
+    "Crops an image using a boolean mask of the same shape."
+    im = np.asarray(im)
+    c = np.nonzero(mask)
+    top_left = np.min(c, axis=1)
+    bottom_right = np.max(c, axis=1)
+    out = im[top_left[0]:bottom_right[0],
+                top_left[1]:bottom_right[1]]
+    return out
