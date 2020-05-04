9a532becda553b6ee192b917d7203e8b80a6521a
Thomas Tumiel <ttumiel@gmail.com>
2020-05-04 22:36:45 +0200

    update imports

--- components/imports.py
+++ components/imports.py
@@ -1,8 +1,18 @@
+# Pytorch
 import torch
 from torch import nn
-import numpy as np
-import matplotlib.pyplot as plt
-from PIL import Image
 import torchvision
 from torchvision import datasets, transforms
 import torch.nn.functional as F
+
+# General
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
+import pandas as pd
+
+# standard lib
+import math
+import random
+from pathlib import Path
+from functools import partial
