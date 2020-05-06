878b4dbe1d34cc5993dd7550204fe64d58004366
Thomas Tumiel <ttumiel@gmail.com>
2020-05-06 20:17:49 +0200

    update mobilenet v2 layers

--- components/models/mobilenetv2.py
+++ components/models/mobilenetv2.py
@@ -9,41 +9,56 @@ class InvertedResidualBlock(nn.Module):
     and the depthwise conv is due to losing representational
     power from the use of a non-linearity, particularly in
     higher dimensions.
+
+    For strides>1 or cin!=cout, the input does not get added to the residual.
     """
-    def __init__(self, cin, cout, expansion_ratio=4, act=nn.ReLU6(True), stride=1):
+    def __init__(self, cin, cout, stride=1, expansion_ratio=6, act=nn.ReLU6(True)):
+        super().__init__()
+        self.stride = stride
+        self.same_c = cin == cout
+        self.block = nn.Sequential(
+            PointWiseConv2d(cin, cin*expansion_ratio),
+            nn.BatchNorm2d(cin*expansion_ratio),
+            act,
+            DepthWiseConv2d(cin*expansion_ratio, st=stride),
+            nn.BatchNorm2d(cin*expansion_ratio),
+            act,
+            PointWiseConv2d(cin*expansion_ratio, cout),
+            nn.BatchNorm2d(cout),
+        )
+
+    def forward(self, x):
+        residual = self.block(x)
+        if self.stride != 1 or not self.same_c:
+            return residual
+        else:
+            return x + residual
+
+
+class ShortcutResidualBlock(nn.Module):
+    """
+    A generalisable residual block that uses a pointwise conv
+    and pooling in the shortcut connection so that the input can
+    be added to the residual.
+    """
+    def __init__(self, cin, cout, stride=1, expansion_ratio=6, act=nn.ReLU6(True)):
         super().__init__()
         self.block = nn.Sequential(
             PointWiseConv2d(cin, cin*expansion_ratio),
-            act, # Does this go here??
             DepthWiseConv2d(cin*expansion_ratio, st=stride),
             act,
-            PointWiseConv2d(cin*expansion_ratio, cin),
+            PointWiseConv2d(cin*expansion_ratio, cout),
+            act
         )
 
-        if stride != 1:
-            self.downsample = nn.AvgPool2d(2, stride=stride)
+        self.downsample = nn.AvgPool2d(2, stride=stride) if stride != 1 else None
+        self.bottle = PointWiseConv2d(cin, cout) if cin != cout else None
 
     def forward(self, x):
         residual = self.block(x)
         if self.downsample is not None:
             x = self.downsample(x)
+
+        if self.bottle is not None:
+            x = self.bottle(x)
         return x + residual
-
-
-class InvertedResidualBlockWithStride(nn.Module):
-    """
-    Inverted residual block with stride doesn't
-    use a residual connection from mobilenetv2.
-    """
-    def __init__(self, cin, cout, expansion_ratio=4, act=nn.ReLU6(True), stride=2):
-        super().__init__()
-        self.block = nn.Sequential(
-            PointWiseConv2d(cin, cin*expansion_ratio),
-            DepthWiseConv2d(cin*expansion_ratio, st=stride),
-            act,
-            PointWiseConv2d(cin*expansion_ratio, cin),
-            act
-        )
-
-    def forward(self, x):
-        return self.block(x)
