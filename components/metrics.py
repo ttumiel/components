2c79eb193ced748c53af17cff1ab0075db589181
Thomas Tumiel <ttumiel@gmail.com>
2020-09-07 12:13:46 +0200

    wrap metrics

--- components/metrics.py
+++ components/metrics.py
@@ -1,17 +1,40 @@
 import torch
 from sklearn.metrics import cohen_kappa_score
+from functools import partial
 
 
+def wrap_metric(metric, detach=True, clone=False, cpu=False, numpy=False, pred_func=None, target_func=None):
+    """Decorates a metric function and applies various transforms to the
+    predictions and targets passed into the function.
+    """
+    def _inner(p, y):
+        if detach: p,y = p.detach(),y.detach()
+        if clone: p,y = p.clone(),y.clone()
+        if cpu: p,y = p.cpu(),y.cpu()
+        if numpy: p,y = p.numpy(),y.numpy()
+        if pred_func is not None:
+            p = pred_func(p)
+        if target_func is not None:
+            y = target_func(y)
+        return metric(p,y)
+
+    _inner.__name__ = metric.__name__
+    return _inner
+
+
+@wrap_metric
 def accuracy(preds, target):
     """
     The average number of correct classes. Only useful
     if the classes are balanced and mostly independent:
     something like ImageNet classification.
     """
+    if preds.ndim>1: preds = preds.argmax(1)
     assert preds.shape == target.shape, f"preds.shape ({preds.shape}) != target.shap ({target.shape})"
     return torch.mean((preds==target).float())
 
 
+@partial(wrap_metric, cpu=True, numpy=True)
 def quadratic_kappa(preds, target):
     """
     Quadratic kappa score. Used for regression tasks to quadratically penalize
@@ -19,4 +42,4 @@ def quadratic_kappa(preds, target):
     observer variability. See https://en.wikipedia.org/wiki/Cohen%27s_kappa
     """
     assert preds.shape == target.shape, f"preds.shape ({preds.shape}) != target.shap ({target.shape})"
-    return torch.tensor(cohen_kappa_score(preds.detach().cpu().numpy(), target.detach().cpu().numpy(), weights='quadratic'))
+    return torch.tensor(cohen_kappa_score(preds, target, weights='quadratic'))
